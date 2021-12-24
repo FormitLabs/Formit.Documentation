9a26f8e99fa2cb04d4b045ad2e69a1c6762ba130
Anaksan <sashakupov9@gmail.com>
2021-12-24 14:00:53 +0500

    изменен favicon, исправлены ошибки связанные с билдом

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/favicon.svg",
   organizationName: "facebook", // Usually your GitHub org/user name.
   projectName: "docusaurus", // Usually your repo name.
 
@@ -50,7 +50,7 @@ const config = {
             items: [
               {
                 label: "Tutorial",
-                to: "/docs/Добро%20пожаловать/Как%20начать",
+                to: "/docs/Добро пожаловать/Как начать",
               },
             ],
           },
