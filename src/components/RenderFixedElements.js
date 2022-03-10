770aa5070cab836d7e0204fac86faafabc4cca38
45den450 <48979302+45den450@users.noreply.github.com>
2022-03-10 11:11:39 +0500

    new component RenderFixedElements

---
+++ src/components/RenderFixedElements.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './RenderFixedElements.module.css';
+
+export default function RenderFixedElements() {
+    return (
+        <div>
+            <div className={styles["ellipse-left"]}></div>
+            <div className={styles["ellipse-top"]}></div>
+            <div className={styles["ellipse-right-owner"]}>
+                <div className={styles["ellipse-right"]}></div>
+                <img src="/img/pointsBottom.svg" alt='' className={styles["points-bottom"]}></img>
+            </div>
+            <img src="/img/pointsTop.svg" alt='' className={styles["points-top"]}></img>
+        </div>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 import HomepageHeader from '../components/HomepageHeader';
+import RenderFixedElements from '../components/RenderFixedElements';
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -14,14 +13,7 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <div className={styles.layout}>
-        <div className={styles["ellipse-left"]}></div>
-        <div className={styles["ellipse-top"]}></div>
-        <div className={styles["ellipse-right-owner"]}>
-          <div className={styles["ellipse-right"]}></div>
-          <img src="/img/pointsBottom.svg" alt='' className={styles["points-bottom"]}></img>
-        </div>
-        <img src="/img/pointsTop.svg" alt='' className={styles["points-top"]}></img>
-
+        <RenderFixedElements />
         <HomepageHeader />
         <main>
           <HomepageFeatures />
