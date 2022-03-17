9d545e9f83aa2b6aff2788b654fe328518ee7ce5
45den450 <48979302+45den450@users.noreply.github.com>
2022-03-17 10:00:36 +0500

    flex -> grid

--- src/components/HomepageHeader.js
+++ src/components/HomepageHeader.js
@@ -2,21 +2,38 @@ import React from 'react';
 import Link from '@docusaurus/Link';
 import styles from './HomepageHeader.module.css';
 
+// export default function HomepageHeader() {
+//     return (
+//       <header className={styles.header}>
+//         <div className={styles["header-first-columns"]}>
+//           <h1 className={styles["header-text"]}>Библиотека знаний Altec<span className="selected-text">Insolations</span></h1>
+//           <Link
+//             className={styles["button-for-resources"]}
+//             to="/docs/Добро пожаловать/Как начать">
+//             <span>Перейти к ресурсам</span>
+//           </Link>
+//         </div>
+//         <div className={styles["video-wrapper"]}>
+//           <div className={styles.rectangle}/>
+//           <img className={styles.video} src="/img/video.png"/>
+//         </div>
+//       </header>
+//     );
+//   }
+
 export default function HomepageHeader() {
-    return (
-      <header className={styles.header}>
-        <div className={styles["header-first-columns"]}>
-          <h1 className={styles["header-text"]}>Библиотека знаний Altec<span className="selected-text">Insolations</span></h1>
-          <Link
-            className={styles["button-for-resources"]}
-            to="/docs/Добро пожаловать/Как начать">
-            <span>Перейти к ресурсам</span>
-          </Link>
-        </div>
-        <div className={styles["video-wrapper"]}>
-          <div className={styles.rectangle}/>
-          <img className={styles.video} src="/img/video.png"/>
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className={styles.header}>
+      <h1 className={styles["header-text"]}>Библиотека знаний Altec<span className="selected-text">Insolations</span></h1>
+      <Link
+        className={styles["button-for-resources"]}
+        to="/docs/Добро пожаловать/Как начать">
+        <span>Перейти к ресурсам</span>
+      </Link>
+      <div className={styles["video-wrapper"]}>
+        <div className={styles.rectangle} />
+        <img className={styles.video} src="/img/video.png" />
+      </div>
+    </header>
+  );
+}
