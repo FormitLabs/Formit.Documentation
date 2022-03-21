af9b114f6109af51e72ea83138e6da97f87fa2e4
45den450 <48979302+45den450@users.noreply.github.com>
2022-03-21 10:36:09 +0500

    fix FixedElements

--- src/components/RenderFixedElements.js
+++ src/components/RenderFixedElements.js
@@ -6,10 +6,10 @@ export default function RenderFixedElements() {
         <div className={styles["fixed-elements"]}>
             <div className={styles["ellipse-left"]}></div>
             <div className={styles["ellipse-top"]}></div>
-            <div className={styles["ellipse-right-owner"]}>
-                <div className={styles["ellipse-right"]}></div>
-                <img src="/img/pointsBottom.svg" alt='' className={styles["points-bottom"]}></img>
-            </div>
+            <div className={styles["ellipse-right"]}></div>
+            <img src="/img/pointsBottom.svg" alt='' className={styles["points-bottom"]}></img>
+            {/* <div className={styles["ellipse-right-owner"]}>
+            </div> */}
             <img src="/img/pointsTop.svg" alt='' className={styles["points-top"]}></img>
         </div>
     );
