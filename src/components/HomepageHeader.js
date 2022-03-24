266da98d1815064d9637c55bbe6c1b12b78d9cd6
45den450 <48979302+45den450@users.noreply.github.com>
2022-03-24 16:09:46 +0500

    fix: video and bugs (#17)
    - Добавлено видео
    - Исправлена стрелка в navbar
    - Исправлен footer для телефона
    - Изменены размеры для телефона и планшета
    
    AI-532
    AI-533
    AI-534
    AI-535
    AI-536

--- src/components/HomepageHeader.js
+++ src/components/HomepageHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
 import styles from './HomepageHeader.module.css';
+import ReactPlayer from "react-player/lazy";
 
 export default function HomepageHeader() {
   return (
@@ -13,7 +14,15 @@ export default function HomepageHeader() {
       </Link>
       <div className={styles["video-wrapper"]}>
         <div className={styles.rectangle} />
-        <img className={styles.video} src="/img/video.png" />
+        <ReactPlayer
+          className={styles.video}
+          width="inherit"
+          height="inherit"
+          url="https://www.youtube.com/watch?v=jWUSUxqEsqM"
+          controls={true}
+          light={true}
+          playing={true}
+        />
       </div>
     </header>
   );
