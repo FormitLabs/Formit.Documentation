cbd2730ac5cd3ec213a2f99d1b51dd7c2dfcbd1a
Sc222 <v.a.shaternikov@gmail.com>
2022-06-13 23:23:48 +0500

    Add new image component

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
+import Image from '@site/src/components/Image';
+
+export default {
+  ...MDXComponents,
+  image: Image,
+};
