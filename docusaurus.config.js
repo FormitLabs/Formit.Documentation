1d8ffbdcacee5636bbdc9e950780c3009f033812
Vladislav Shaternikov <v.a.shaternikov@gmail.com>
2023-01-11 14:34:43 +0500

    Fix wrong url (#61)
    Некорректная ссылка на "Разработка Altec" в футере

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -100,7 +100,7 @@ const config = {
               },
               {
                 html: `
-                    <a href="https://altecsystems.ru/insolations/" class="footer-link" target="_blank" rel="noopener noreferrer">
+                    <a href="https://bim.insolations.ru/" class="footer-link" target="_blank" rel="noopener noreferrer">
                       <img src="/img/linkIcon.svg" alt="Изображение сторонней ссылки" class="footer-link-image"/>Разработка Altec
                     </a>
                   `,
