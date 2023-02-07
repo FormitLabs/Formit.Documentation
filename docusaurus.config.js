85ff458e89f1fcee87e8b22b4a74c58bbc99e753
Sc222 <v.a.shaternikov@gmail.com>
2023-02-07 15:59:03 +0500

    Update what's new link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
         items: [
           { label: "Руководство пользователя", to: "/docs/manual/registration", className: "nav-first-item" },
           { label: "Методические указания", to: "/docs/guidelines/possibilities", className: "nav-other-item" },
-          { label: "Что нового", to: "/docs/changelog/19-12-2022", className: "nav-other-item" },
+          { label: "Что нового", to: "/docs/changelog/07-02-2023", className: "nav-other-item" },
           { label: "Ответы на вопросы", to: "/docs/questions", className: "nav-other-item" },
           { label: "Корпоративный сайт", to: "https://altecsystems.ru/", className: "nav-link-item" },
           { label: "Из 2D в BIM", to: "https://altecsystems.ru/2d-to-bim", className: "nav-link-item" },
@@ -119,7 +119,7 @@ const config = {
               },
               {
                 label: "Что нового",
-                to: "/docs/changelog/19-12-2022",
+                to: "/docs/changelog/07-02-2023",
               },
               {
                 label: "Ответы на вопросы",
