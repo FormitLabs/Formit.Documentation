0f488055846aadc96dfa99ff89228c76e05e21ef
Tany Krestnikova <krestnikovatat@gmail.com>
2024-04-04 18:20:52 +0500

    Ро среднее, ро ф и тау записаны через sub

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Image({width, image, title}){
+export default function Image({width, image, title, altOverride}){
     return(
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "16px" }}>
-        <img src={image} alt={title} width={width} style={{ textAlign: "center" }} />
+        <img src={image} alt={altOverride??title} width={width} style={{ textAlign: "center" }} />
         {title && <i>{title}</i>}
     </div>
     );
