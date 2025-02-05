08ab8be2e49b557992626e78d22cdcab76cb187f
Vlad <v.a.shaternikov@gmail.com>
2025-02-05 17:20:29 +0500

    feat: Ребрендинг на формит, актуализация ссылок на соц. сети, удаление ненужных разделов и небольшое улучшение верстки лендинга #83

--- src/components/HomepageHeader.js
+++ src/components/HomepageHeader.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import Link from '@docusaurus/Link';
-import styles from './HomepageHeader.module.css';
-import ReactPlayer from "react-player/lazy";
+import React from "react";
+import Link from "@docusaurus/Link";
+import styles from "./HomepageHeader.module.css";
 
 export default function HomepageHeader() {
   return (
     <header className={styles.header}>
-      <h1 className={styles["header-text"]}>Библиотека знаний Altec<span className="selected-text">Insolations</span></h1>
-      <Link
-        className={styles["button-for-resources"]}
-        to="/docs/">
+      <h1 className={styles["header-text"]}>
+        Библиотека знаний
+        <br />
+        <span className="selected-text">Formit</span>
+      </h1>
+      <Link className={styles["button-for-resources"]} to="/docs/">
         <span>Перейти к ресурсам</span>
       </Link>
-      <div className={styles["video-wrapper"]}>
+      {/* TODO: загрузить видео в вк или на рутуб */}
+      {/*<div className={styles["video-wrapper"]}>
         <div className={styles.rectangle} />
         <ReactPlayer
           className={styles.video}
           width="inherit"
           height="inherit"
           url="https://www.youtube.com/watch?v=jWUSUxqEsqM"
-          controls={true}
-          light={true}
-          playing={true}
+          controls
+          light
+          playing
         />
-      </div>
+      </div>*/}
     </header>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Layout
       title="Библиотека знаний"
-      description="Руководство по работе с AltecInsolations - программным комплексом для расчета инсоляции и КЕО">
+      description="Руководство по работе с Formit - программным комплексом для расчета инсоляции и КЕО">
       <div className={styles.layout}>
         <RenderFixedElements />
         <HomepageHeader />
