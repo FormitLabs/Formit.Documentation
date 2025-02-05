2fcd5ddf585d6ccad06463f0aea95bc1c2fc7069
Sc222 <v.a.shaternikov@gmail.com>
2025-02-05 16:50:40 +0500

    Change titles to formit+ Hide youtube video + Better landing page markup (still bad)

--- src/components/RenderCommunities.js
+++ src/components/RenderCommunities.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import styles from './RenderCommunities.module.css';
+import React from "react";
+import styles from "./RenderCommunities.module.css";
 
-const communityList = [
-    {
-      src: '/img/tgIcon.svg',
-      description: (
-        <>
-          Мы активно развиваем сообщество AltecInsolations в <a href="https://t.me/altecinsolations" target="_blank" rel="noopener noreferrer" className="selected-text">Telegram</a>-канале.
-          Подписывайтесь, чтобы ничего не пропустить!
-        </>
-      )
-    },
-  ];
-  
-  function Community({src, description}) {
-    return (
-      <div className={styles["community-container"]}>
-        <img src={src} alt={description}/>
-        <p className={styles["community-description"]}>{description}</p>
-      </div>
-    );
-  }
+function Community({ src, alt, children }) {
+  return (
+    <div className={styles["community-container"]}>
+      <img src={src} alt={alt} />
+      <p className={styles["community-description"]}>{children}</p>
+    </div>
+  );
+}
 
 export default function RenderCommunities() {
-    return (
-        <div className={styles.communities}>
-            {communityList.map((props, idx) => (
-                <Community key={`community ${idx}`} {...props} />
-            ))}
-        </div>
-    );
+  return (
+    <div className={styles.communities}>
+      <Community src="/img/tgIcon.svg" alt="Логотип Telegram">
+        Мы активно развиваем сообщество <b>Formit</b> в&nbsp;
+        <a
+          href="https://t.me/altecformit"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="selected-text"
+        >
+          Telegram
+        </a>
+        .
+        <br />
+        Подписывайтесь, чтобы ничего не&nbsp;пропустить!
+      </Community>
+    </div>
+  );
 }
