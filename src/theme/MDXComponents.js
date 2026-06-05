00182185c13838d6fd8046abc48490c0add7592f
Sc222 <sc222work@gmail.com>
2026-06-06 03:36:36 +0500

    Damn we need prettier

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,5 +1,5 @@
-import MDXComponents from '@theme-original/MDXComponents';
-import Image from '@site/src/components/Image';
-import DashedDivider from "../components/DashedDivider";
+import MDXComponents from "@theme-original/MDXComponents";
+import Image from "@site/src/components/Image";
+import DashedDivider from "@site/src/components/DashedDivider";
 
 export default {...MDXComponents, Image, DashedDivider};
